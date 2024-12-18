ee027a60e919c3c262e6877d311fc79b0c65fdbb
Phu2885 <74325132+Phu2885@users.noreply.github.com>
2024-12-18 20:39:20 +0300

    Update abstract.py

--- LAB/lab2/lab_python_oop/abstract.py
+++ LAB/lab2/lab_python_oop/abstract.py
@@ -1,1 +1,16 @@
+import abc
+
+
+class figure(abc.ABC):
+    @abc.abstractmethod
+    def square(self):
+        pass
+
+    @abc.abstractmethod
+    def repr(self):
+        pass
+
+    @abc.abstractmethod
+    def get_name(self):
+        pass
 
